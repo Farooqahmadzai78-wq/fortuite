c2cd19da37d120bb9ffceab253bb4964a05c4607
Farooqahmadzai78-wq <farooqahmadzai78@gmail.com>
2026-08-27 16:07:35 +0200

    Update app name and server configuration

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,13 +2,17 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'com.islamnoor.app',
-  appName: 'Islam-Noor',
+  appName: 'Islam Noor',
   webDir: '.output/public',
-  plugins: {
-    PushNotifications: {
-      presentationOptions: ['badge', 'sound', 'alert'],
-    },
+  server: {
+    androidScheme: 'https'
   },
+  plugins: {
+    LocalNotifications: {
+      smallIcon: "ic_stat_icon_config_sample",
+      iconColor: "#488AFF",
+    }
+  }
 };
 
 export default config;
